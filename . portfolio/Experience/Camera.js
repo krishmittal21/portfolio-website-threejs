9eb50d686dbdf3346bee8cff9213725665f02819
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-31 14:35:02 +0530

    Update Camera.js

--- . portfolio/Experience/Camera.js
+++ . portfolio/Experience/Camera.js
@@ -5,5 +5,12 @@ export default class Camera{
         this.sizes = this.experience.sizes;
         this.scene=this.experience.scene;
         this.canvas = this.experience.canvas;
+        this.createPerspectiveCamera();
+        this.createOrthographicCamera();
+    }
+    createPerspectiveCamera(){
+        this.perspectiveCamera = new THREE.PerspectiveCamera(75,this.sizes.width/this.sizes.height,0.1,100);
+        this.perspectiveCamera.position.z = 3;
+        this.scene.add(this.perspectiveCamera);
     }
 }
