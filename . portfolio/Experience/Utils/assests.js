3a1d4230a399978604b4d129fda5b65536d5aac6
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-03 21:43:28 +0530

    update assests.js

---
+++ . portfolio/Experience/Utils/assests.js
@@ -0,0 +1,12 @@
+export default[
+    {
+        name: "me",
+        type:"glbModel",
+        path: "/models/avatar.glb",
+    },
+    {
+        name:"walle",
+        type:"glbModel",
+        path: "/models/walle.glb",
+    }
+]
