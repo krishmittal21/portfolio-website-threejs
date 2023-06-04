084cff83637580a0ff672354a6846c3e818eaeed
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-04 19:48:53 +0530

    update Sizes.js

--- . portfolio/Experience/Utils/Resources.js
+++ . portfolio/Experience/Utils/Resources.js
@@ -4,7 +4,7 @@ import * as THREE from "three"
 export default class Resources extends EventEmitter{
     constructor(assests){
         super();
-        this.experience = new Experience()
+        this.experience = new Experience();
         this.renderer= this.experience.renderer
         this.assests = assests;
         console.log(this.assests)
--- . portfolio/Experience/Utils/Sizes.js
+++ . portfolio/Experience/Utils/Sizes.js
@@ -1,5 +1,4 @@
 import {EventEmitter} from "events"
-import * as THREE from "three"
 export default class Sizes extends EventEmitter{
     constructor(){
         super();
@@ -7,12 +6,25 @@ export default class Sizes extends EventEmitter{
         this.height = window.innerHeight
         this.aspect = this.width / this.height
         this.pixelRatio = Math.min(window.devicePixelRatio,2)
+        this.frustrum = 5;
+        if (this.width < 968) {
+            this.device = "mobile";
+        } else {
+            this.device = "desktop";
+        }
         window.addEventListener('resize', ()=>{
             this.width = window.innerWidth;
             this.height = window.innerHeight;
             this.aspect = this.width / this.height;
             this.pixelRatio = Math.min(window.devicePixelRatio,2);
             this.emit('resize')
+            if (this.width < 968 && this.device !== "mobile") {
+                this.device = "mobile";
+                this.emit("switchdevice", this.device);
+            } else if (this.width >= 968 && this.device !== "desktop") {
+                this.device = "desktop";
+                this.emit("switchdevice", this.device);
+            }
         })           
     }
 }
