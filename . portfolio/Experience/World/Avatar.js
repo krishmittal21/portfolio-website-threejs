691079f78a818fe17c54d099d9ef2e4d0d91a2a9
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-06-16 19:32:48 +0530

    Update Avatar.js

--- . portfolio/Experience/World/Avatar.js
+++ . portfolio/Experience/World/Avatar.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import Experience from "../Experience.js";
 export default class Avatar {
     constructor() {
@@ -13,6 +14,30 @@ export default class Avatar {
         this.scene.add(this.actualAvatar);
         this.actualAvatar.scale.set(0.5,0.5,0.5);
         this.actualAvatar.position.set(0,0,0);
+    }
+    mousePosition(){
+        const mouse = new THREE.Vector2();
+        window.addEventListener('mousemove',function(e) {
+            mousePosition.x=(e.clientX/window.innerWidth)*2-1;
+            mousePosition.y=-(e.clientY/window.innerHeight)*2+1;
+        })
+        const planeGeo = new THREE.PlaneGeometry(25,25);
+        const planeMat = new THREE.MeshBasicMaterial({visible:false});
+        const planeMesh = new THREE.Mesh(planeGeo,planeMat);
+        planeMesh.rotation.x =-0.5 * -Math.PI/2;
+        this.scene.add(planeMesh);
+        planeMesh.name = 'plane';
+        const raycaster = new THREE.Raycaster();
+        window.addEventListener('click',function(){
+            raycaster.setFromCamera(mousePosition,camera);
+            const intersects = raycaster.intersectObjects(this.scene.children);
+            for(let i=0; i<intersects.length;i++){
+                if(intersects[i].object.name === 'plane'){
+                    target.position.set(intersects[i].point.x,0,intersects[i].point.z)
+                }
+            }
+            
+        })
     }
     resize() {   
     }
